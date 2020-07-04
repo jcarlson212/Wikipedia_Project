2385436fa9ec11c0da2d12ea07c816312e119086
Jason Carlson <jcarlson212@gmail.com>
2020-07-04 03:24:32 -0700

    Now users can choose a random entry and edit entries

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<str:title>", views.entryPage, name="entry"),
     path("results/<str:query>", views.results, name="results"),
-    path("create/newEntry", views.create, name="create")
+    path("create/newEntry", views.create, name="create"),
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("random/random", views.randomPage, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from . import util
+import random
 
 
 
@@ -49,3 +50,33 @@ def create(request):
             })
     return render(request, "encyclopedia/create.html")
 
+def edit(request, title):
+    if request.method == "POST":
+        entry = request.POST["entry"]
+        util.save_entry(title, entry)
+        print("dsfsdf")
+        print(title)
+        print(title)
+        print(title)
+        return HttpResponseRedirect("/" + title)
+    print("sdfsdf")
+    entry = util.get_entry(title)
+    if entry != None:
+        return render(request, "encyclopedia/edit.html", {
+            "entry": entry,
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "error": "Entry not found."
+        })
+
+def randomPage(request):
+    elements = util.list_entries()
+    randNum = random.randint(0, len(elements) - 1)
+    title = elements[randNum]
+    return entryPage(request, title)
+    return render(request, "encyclopedia/entry.html", {
+        "entry": util.get_entry(title),
+        "title": title
+    })
